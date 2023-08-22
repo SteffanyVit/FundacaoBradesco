16dd78196f7049a7f8c28139a07ab55d88e94a65
Steffany Vitória da Silva <142359224+SteffanyVit@users.noreply.github.com>
2023-08-22 23:47:31 +0000

    at2

--- Linguagensdeprogramação/vetor.py
+++ Linguagensdeprogramação/vetor.py
@@ -1,5 +1,7 @@
 v = []
 i = 0
+soma = 0
+
 while (i<20):
     v.append(float(input('digite a nota: ')))
     i = i+1
@@ -7,6 +9,9 @@ while (i<20):
 i=0
 print('notas>7.5:')
 while(i<20):
+    soma = soma + v
+    media = soma / v
+    print media
     if v[i]>7.5:
         print(v[i])
     i=i + 1
